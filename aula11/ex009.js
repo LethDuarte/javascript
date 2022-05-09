1a63145f6c353d04c71c07f5c2d1c1b047350a76
Letícia Duarte Rosa <lethduarte@gmail.com>
2022-05-09 17:37:25 -0300

    adicionada aula 011

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Br'
+
+console.log (`Nascido em ${pais},`)
+
+if (pais == 'Brasil') { //if (pais != Brasil)... estrangeiro
+    console.log('você é brasileiro!')
+} else { // ...brasileiro
+    console.log ('você é estrangeiro...')
+}
